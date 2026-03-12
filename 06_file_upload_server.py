e301db7b6a08810c527d9eaaa1a7fa9506c82a5e
Trower <troweredith@gmail.com>
2026-03-12 16:44:00 +0800

    新建程序：上传文件——服务器端

---
+++ 06_file_upload_server.py
@@ -0,0 +1,21 @@
+import socket
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+server_socket.bind(("", 10086))
+
+server_socket.listen(5)
+
+accept_socket, client_info = server_socket.accept()
+
+with open('./data/my.txt', 'wb') as dest_f:
+    while True:
+        bys = accept_socket.recv(8192)
+
+        if len(bys) == 0:
+            break
+
+        dest_f.write(bys)
+
+
+accept_socket.close()
