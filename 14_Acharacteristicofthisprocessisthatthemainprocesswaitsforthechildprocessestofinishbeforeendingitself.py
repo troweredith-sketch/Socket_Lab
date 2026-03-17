d91550662fa22ec9c218ea2bb63e5fd84c1577a2
Trower <troweredith@gmail.com>
2026-03-17 20:22:03 +0800

    改成用join阻塞主进程

--- 14_Acharacteristicofthisprocessisthatthemainprocesswaitsforthechildprocessestofinishbeforeendingitself.py
+++ 14_Acharacteristicofthisprocessisthatthemainprocesswaitsforthechildprocessestofinishbeforeendingitself.py
@@ -8,7 +8,12 @@ def work():
 
 if __name__ == "__main__":
     p1 = multiprocessing.Process(target=work, name='WorkerProcess')
+    p1.daemon = True   # 设置p1为守护进程
+
     p1.start()
     
     time.sleep(1)  # 主进程等待一段时间，确保子进程已经开始工作
-    print("main进程结束了，但子进程还在工作...")
+    print("主进程原本准备结束，但由于使用了 join()，现在会等待子进程执行完毕...")
+
+    p1.join()  # 阻塞主进程，直到 p1 执行完毕
+    
