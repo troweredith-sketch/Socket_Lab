b652c66135bd982832489b266f640dc8f6e9fad6
troweredith-sketch <troweredith@gmail.com>
2026-03-17 09:21:01 +0800

    服务器端ip修改

--- 06_fileUpload_server.py
+++ 06_fileUpload_server.py
@@ -2,7 +2,7 @@ import socket
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-server_socket.bind(("127.0.0.1", 10086))
+server_socket.bind(("", 10086))
 
 server_socket.listen(5)
 
